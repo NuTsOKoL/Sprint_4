b3ac4a7d5cb3b4ebedcd25d624546fbf6facdb8e
NuTsOKoL <mi.sei@yandex.ru>
2024-03-03 10:19:10 +0300

    1

---
+++ MovieQuiz-ios/MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIColor {
+    static var yppBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var yppRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var uppBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var uppBackground: UIColor { UIColor(named: "UP Background") ?? UIColor.darkGray }
+    static var uppGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var uppWhite: UIColor { UIColor(named: "UP White") ?? UIColor.white }
+}
